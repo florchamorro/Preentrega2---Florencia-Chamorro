15744eccc88d50325fe80e6cba5cea5e11c0ed98
florchamorro <104289980+florchamorro@users.noreply.github.com>
2023-11-23 21:56:56 -0300

    first commit

---
+++ src/components/Logo/imagenLogo.jsx
@@ -0,0 +1,9 @@
+import {PerfumeriaOrquidea} from '../NavBar/NavBar.module.css'
+const Logo = () => {
+    return (
+        <div>
+            <a href="#"><img src="../src/assets/img/imagenLogo.jpg" alt="Perfumeria" className={PerfumeriaOrquidea}/></a>
+        </div>
+    );
+  };
+  export default Logo
